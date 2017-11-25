024cb0e964f7cdb6de784523853064c838dec0a0
legogas <46173275@mail.ru>
2017-11-25 21:20:17 +0300

    бла бла бла

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using SFML.Audio;
+
+
+namespace vspGL
+{
+    class Program
+    {
+        public static RenderWindow wind;
+        static void Main(string[] args)
+        {
+            wind = new RenderWindow(new VideoMode(1280, 680), "sfml game");
+
+            wind.Closed += onClose;
+
+            while (wind.IsOpen)
+            {
+                wind.DispatchEvents();
+
+                wind.Clear(Color.White);
+
+                // draving
+
+                wind.Display();
+            }
+        }
+
+        private static void onClose(object sender, EventArgs e)
+        {
+            wind.Close();
+        }
+    }
+}
